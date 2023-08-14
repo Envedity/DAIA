4ced18a6151898ed254269b718f8266a045e0fa3
Paillat <me@paillat.dev>
2023-08-14 18:37:43 +0200

    :art: chore(*): run black to format the code

--- src/auto_gpt_plugin_template/__init__.py
+++ src/auto_gpt_plugin_template/__init__.py
@@ -1,19 +1,22 @@
-
 from typing import Any, Dict, List, Optional, Tuple, TypedDict, TypeVar
 from auto_gpt_plugin_template import AutoGPTPluginTemplate
 
 PromptGenerator = TypeVar("PromptGenerator")
 
+
 class Message(TypedDict):
     role: str
     content: str
+
 
 class AutoGPTScreenVisuals(AutoGPTPluginTemplate):
     def __init__(self):
         super().__init__()
         self._name = "autogpt-screen-visuals"
         self._version = "0.1.0"
-        self._description = "Integration for screen visual representation and interactions."
+        self._description = (
+            "Integration for screen visual representation and interactions."
+        )
 
     def get_screen_visuals(self):
         # TODO: Implement DVAI call to get screen representation
@@ -25,22 +28,23 @@ class AutoGPTScreenVisuals(AutoGPTPluginTemplate):
 
     def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
         prompt.add_command(
-            "get_screen_visuals", 
-            "Fetch a representation of the screen with IDs for elements.", 
+            "get_screen_visuals",
+            "Fetch a representation of the screen with IDs for elements.",
             {},
-            self.get_screen_visuals
+            self.get_screen_visuals,
         )
         prompt.add_command(
-            "click_on_visual", 
-            "Simulate a click on a screen element by ID and return updated screen representation.", 
+            "click_on_visual",
+            "Simulate a click on a screen element by ID and return updated screen representation.",
             {"visual_id": "<visual_id>"},
-            self.click_on_visual
+            self.click_on_visual,
         )
         return prompt
-    
+
     """
     Capability functions
     """
+
     def can_handle_post_prompt(self) -> bool:
         return True
 
@@ -79,10 +83,11 @@ class AutoGPTScreenVisuals(AutoGPTPluginTemplate):
 
     def can_handle_user_input(self) -> bool:
         return False
-    
+
     """
     Placeholder methods DO NOT REMOVE
     """
+
     def on_response(self, response: str, *args, **kwargs) -> str:
         pass
 
