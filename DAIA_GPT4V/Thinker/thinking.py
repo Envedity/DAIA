8fbe3eb3fc64d25ab4de618e8bafba44a27b1e58
Code7G <111247923+Code7G@users.noreply.github.com>
2023-11-26 16:20:58 +0100

    Applied licence to files

--- DAIA_GPT4V/Thinker/thinking.py
+++ DAIA_GPT4V/Thinker/thinking.py
@@ -1,3 +1,16 @@
+#    DAIA -  Digital Artificial Inteligence Agent
+#    Copyright (C) 2023  Envedity
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
 from DAIA_GPT4V.Memory.memory import Memory
 from DAIA_GPT4V.OS_control.os_controller import OSController
 from DAIA_GPT4V.DVAI.GPT_4_with_Vision import DVAI
